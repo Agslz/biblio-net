0e6a15f0ad8deb698f108bd78c5426d868576959
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-15 13:26:49 -0300

    Adding repositories

---
+++ book-network/src/main/java/com/ags/book/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.ags.book.repository;
+
+import com.ags.book.role.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findByName(String role);
+
+}
