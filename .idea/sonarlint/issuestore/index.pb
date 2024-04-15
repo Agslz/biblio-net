b732809a16422252a87754b03968d89479856c44
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-15 10:32:03 -0300

    First commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+f
+6book-network/src/main/resources/application.properties,a\a\aa08752d93215d112ec27428ec8ecea376f9b1fc
+D
+book-network/pom.xml,d\a\da6427f4a852e69b5f46de706f298cb51d7db379
---
+++ book-network/src/test/java/com/ags/book/BookNetworkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookNetworkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
