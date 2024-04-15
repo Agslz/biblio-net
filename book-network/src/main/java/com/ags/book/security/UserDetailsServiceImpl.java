869b8faa7940c5357dd5a4b743fe824ed4c2ccb9
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-15 18:57:49 -0300

    Adding security

---
+++ book-network/src/main/java/com/ags/book/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.ags.book.security;
+
+import com.ags.book.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow( () -> new UsernameNotFoundException("User not found"));
+    }
+}
