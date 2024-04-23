d55a0085da94b34f547ceae6e6e3458fdab8ba2b
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-23 00:35:41 -0300

    Create OpenApiConfig.java

---
+++ book-network/src/main/java/com/ags/book/config/OpenApiConfig.java
@@ -0,0 +1,50 @@
+package com.ags.book.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Agustin Schulze",
+                        email = "agsschulze@gmail.com",
+                        url = "https://agslz.github.io/portfolio/"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - Agustin Schulze",
+                version = "1.0",
+                license = @License(
+                        name = "License Name",
+                        url = "https://www.apache.org/licenses/LICENSE-2.0"
+                ),
+                termsOfService = "Terms of Service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8088/api/v1"
+                ),
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "BearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "BearerAuth",
+        description = "JWT Auth Description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
