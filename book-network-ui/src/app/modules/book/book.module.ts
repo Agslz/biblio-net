6ed30e1c51be85292750fd238783d74e6ba24202
Agustín Schulze <agsp3rs3o@gmail.com>
2024-04-25 10:05:54 -0300

    Implementing menu and book list pages

--- book-network-ui/src/app/modules/book/book.module.ts
+++ book-network-ui/src/app/modules/book/book.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BookRoutingModule } from './book-routing.module';
 import { MainComponent } from './pages/main/main.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { BookListComponent } from './pages/book-list/book-list.component';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    MenuComponent,
+    BookListComponent
   ],
   imports: [
     CommonModule,
